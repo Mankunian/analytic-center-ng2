f0f0434c31a51fe720351f9043099da86a9855cd
web-dev <dias.07.04@gmail.com>
2020-01-23 18:30:12 +0600

    show list of regions for auth

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
-
-  constructor() { }
+	foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+	
+  constructor() {}
 
   ngOnInit() {
   }
