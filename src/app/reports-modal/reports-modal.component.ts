06d9c12f3ee43c21266f08594d81502e1c9ebc72
Aidos Zhunusbekov <a.zhunusbekov.m@gmail.com>
2020-01-27 14:40:09 +0600

    report modal created

---
+++ src/app/reports-modal/reports-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reports-modal',
+  templateUrl: './reports-modal.component.html',
+  styleUrls: ['./reports-modal.component.scss']
+})
+export class ReportsModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
