b7321807f8723cb6fade70aac667cb52436b359e
web-dev <dias.07.04@gmail.com>
2020-11-18 00:19:08 +0600

    global.ts

--- src/app/global.ts
+++ src/app/global.ts
@@ -3,20 +3,20 @@ let hostname = window.location.hostname;
 sessionStorage.setItem('hostname', hostname);
 
 const sourceURLs = {
-  ADM_PAGE: '',
-  ADM_API_URL:'',
-  BASE_API_URL: window.location.protocol.toString() + "//" + hostname.toString() + ":8081/api/v1/",
-  SOCKET_URL: window.location.protocol.toString() + "//" + hostname.toString() + ":8081",
+	ADM_PAGE: '',
+	ADM_API_URL: '',
+	BASE_API_URL: window.location.protocol.toString() + "//" + hostname.toString() + ":8081/api/v1/",
+	SOCKET_URL: window.location.protocol.toString() + "//" + hostname.toString() + ":8081",
 }
 if (hostname === 'localhost') {
-	sourceURLs.ADM_PAGE =  'http://localhost:4200';
+	sourceURLs.ADM_PAGE = 'http://localhost:4200';
 	sourceURLs.ADM_API_URL = 'https://18.138.17.74:8084';
 	sourceURLs.BASE_API_URL = 'https://18.140.232.52:8081/api/v1/';
-  sourceURLs.SOCKET_URL = 'https://18.140.232.52:8081';
+	sourceURLs.SOCKET_URL = 'https://18.140.232.52:8081';
 }
 else if (hostname.startsWith("10")) {
-  sourceURLs.ADM_PAGE = "http://10.2.30.69";
-  sourceURLs.ADM_API_URL = "http://10.2.30.69:8084";
+	sourceURLs.ADM_PAGE = "http://10.2.30.69";
+	sourceURLs.ADM_API_URL = "http://10.2.30.69:8084";
 }
 else if (hostname.startsWith("192")) {
 	sourceURLs.ADM_PAGE = 'http://192.168.210.69';
@@ -24,9 +24,9 @@ else if (hostname.startsWith("192")) {
 }
 else if (hostname.startsWith("master")) {
 	sourceURLs.ADM_PAGE = 'https://master.d260huhvcvtk4w.amplifyapp.com';
-  sourceURLs.ADM_API_URL = 'https://18.138.17.74:8084';
-  sourceURLs.BASE_API_URL = 'https://18.140.232.52:8081/api/v1/';
-  sourceURLs.SOCKET_URL = 'https://18.140.232.52:8081';
+	sourceURLs.ADM_API_URL = 'https://18.138.17.74:8084';
+	sourceURLs.BASE_API_URL = 'https://18.140.232.52:8081/api/v1/';
+	sourceURLs.SOCKET_URL = 'https://18.140.232.52:8081';
 }
 
 
@@ -34,10 +34,10 @@ else if (hostname.startsWith("master")) {
 
 export const GlobalConfig = Object.freeze({
 
-  BASE_API_URL: sourceURLs.BASE_API_URL,
-  SOCKET_URL: sourceURLs.SOCKET_URL,
-  ADM_URL: sourceURLs.ADM_API_URL,
-  ADMIN_PAGE: sourceURLs.ADM_PAGE,
+	BASE_API_URL: sourceURLs.BASE_API_URL,
+	SOCKET_URL: sourceURLs.SOCKET_URL,
+	ADM_URL: sourceURLs.ADM_API_URL,
+	ADMIN_PAGE: sourceURLs.ADM_PAGE,
 
 
 	BASE_AUTH_USER: "user0",
