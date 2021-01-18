d28922b716fa98f71a7e5bbeae3f654c8e691fdf
web-dev <dias.07.04@gmail.com>
2021-01-18 12:42:18 +0600

    websocket done

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,23 +1,13 @@
-// import { Component, OnDestroy, OnInit } from "@angular/core";
-// import { RxStompService } from "@stomp/ng2-stompjs";
-// import { Stomp } from "@stomp/stompjs";
-// import { Subscription } from "rxjs";
-// import { MessageService } from "primeng/api";
-// import * as SockJS from "sockjs-client";
-
-// import { Message, Stomp } from "@stomp/stompjs";
-
-
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import { Client, Message, Stomp } from "@stomp/stompjs";
-import { Subscription } from "rxjs";
 import { MessageService } from "primeng/api";
-import { RxStompService } from "@stomp/ng2-stompjs";
-import { SharedService } from "../services/shared.service";
-import { ErrorHandlerService } from "../services/error-handler.service";
-import { HttpService } from "../services/http.service";
-import { GlobalConfig } from "../global";
+import { Stomp } from "@stomp/stompjs";
 import * as SockJS from "sockjs-client";
+// import { Subscription } from "rxjs";
+// import { RxStompService } from "@stomp/ng2-stompjs";
+// import { SharedService } from "../services/shared.service";
+// import { ErrorHandlerService } from "../services/error-handler.service";
+// import { HttpService } from "../services/http.service";
+// import { GlobalConfig } from "../global";
 
 
 // eslint-disable-next-line prettier/prettier
@@ -28,66 +18,55 @@ import * as SockJS from "sockjs-client";
 	providers: [MessageService],
 })
 export class MessagesComponent implements OnInit {
-	public receivedMessages: string[] = [];
-	private topicSubscription: Subscription;
-	private subscription: Subscription;
-	private terrCode;
-	private authUser = GlobalConfig.BASE_AUTH_USER;
-	private users;
-	stompClient: any = null;
+	public stompClient;
+	public msg = [];
 
 
-	constructor(
-		// private rxStompService: RxStompService,
-		public shared: SharedService,
-		// private messageService: MessageService,
-		// private http: HttpService,
-		// private errorHandler: ErrorHandlerService
-	) { }
+	ngOnInit() { }
 
-	ngOnInit() {
-		/*
-			Создаем websocket-соединение, используем библиотеку stompjs для работы по протоколу STOMP,
-			также используем библиотеку sockjs для обеспечения для поддержки функционала в браузерах
-			неподдерживающих websocket и для пользователей работающих через прокси
-		*/
-		this.connect();
-		// this.subscribe()
+	constructor() {
+		this.initializeWebSocketConnection();
 	}
 
-	connect() {
+	initializeWebSocketConnection() {
 		const userInfo = JSON.parse(sessionStorage.userInfo);
 		const username = userInfo.fullName;
-		const socket = new SockJS('http://18.138.17.74:8085/notifications/' + username);
-		const stompClient = Stomp.over(socket);
-
-		console.log(stompClient)
-
-
-
-		stompClient.connect({}, function (frame) {
-			console.log(frame)
-			//Проверка связи
-			stompClient.subscribe('/topic/greetings', function (message) {
+		const serverUrl = 'http://18.138.17.74:8085/notifications/' + username;
+		const ws = new SockJS(serverUrl);
+		this.stompClient = Stomp.over(ws);
+		// eslint-disable-next-line @typescript-eslint/no-this-alias
+		const that = this;
+		this.stompClient.connect({}, function (frame) {
+			that.stompClient.subscribe('/topic/greetings', (message) => {
 				console.log(JSON.parse(message.body));
-				let body = message.body;
-				this.shared.getWsGreetings(body)
-			});
+				// if (message.body) {
+				// 	this.msg.push(message.body)
+				// }
+				// console.log(this.msg)
+			})
 
 
-			//Канал для получения данных о проценте формирования срезов
-			stompClient.subscribe('/topic/slice-completion-info', function (message) {
+			that.stompClient.subscribe('/topic/slice-completion-info', (message) => {
 				console.log(JSON.parse(message.body));
-			});
+				that.showToast(message.body)
+				// if (message.body) {
+				// 	this.msg.push(message.body)
+				// }
+			})
 
-			//Канал для получения индивидуальных сообщений
-			stompClient.subscribe('/user/queue/notifications', function (message) {
+			that.stompClient.subscribe('/user/queue/notifications', (message) => {
 				console.log(JSON.parse(message.body));
-			});
-
-			//Если хотим получить приветственное уведомление вызываем сервис sayHello, которому передаем sessionKey
-			stompClient.send('/app/hello', {}, JSON.stringify({ name: username }));
-
+				// if (message.body) {
+				// 	this.msg.push(message.body)
+				// }
+			})
 		})
 	}
+
+	showToast(body) {
+		console.log(body)
+	}
+
+
+
 }
